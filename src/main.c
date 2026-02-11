03e3089e155d7a77f112dae8c44c37406d3b16ef
Muhammad <ansarisaad83299@gmail.com>
2026-02-10 20:56:57 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,10 +3,16 @@
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
-  //setbuf(stdout, NULL);
+  setbuf(stdout, NULL);
 
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  char command[100];
+  fgets(command, sizeof(command), stdin);
+  printf("%s", command);
+  printf(": command not found");
+
+
 
   return 0;
 }
