26ddd1c0e31a6809a3b64d681757c0d866cea9e9
Muhammad <ansarisaad83299@gmail.com>
2026-02-11 01:53:15 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,26 +6,31 @@
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
   char command_line[1024];
-  char args[1024];
+  
 
   while(true)
   {  
     printf("$ ");
     fgets(command_line, sizeof(command_line), stdin);
     command_line[strcspn(command_line, "\n")] = '\0';
-    size_t idx = strcspn(command_line, " ");
-    command_line[idx] = '\0';
-    args[0] = command_line[idx + 1];
 
+    char *command = strtok(command_line, " ");
+    char *args = strtok(NULL, " ");
+    
 
-    if(strcmp(command_line, "exit") == 0){
+    if(strcmp(command, "exit") == 0){
       break;
     }
-    else if(strcmp(command_line, "echo") == 0){
-      printf("%s\n", args);
+    else if((strcmp(command, "echo") == 0) && (args != NULL)){
+      printf("\n");
+      while(args != NULL){
+        printf("%s", args);
+        args = strtok(NULL, " ");
+      }
+      printf("\n");
     }
     else{
-      printf("%s: command not found\n", command_line);
+      printf("%s: command not found\n", command);
     }
   }
 
